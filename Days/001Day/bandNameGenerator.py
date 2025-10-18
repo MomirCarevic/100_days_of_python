d68da994fbf265fe0df96beaf2ff26c43f07de8d
MomirCarevic <carevic.momir00@gmail.com>
2025-10-18 21:27:27 +0200

    Finished Day 1

--- Days/001Day/bandNameGenerator.py
+++ Days/001Day/bandNameGenerator.py
@@ -1,6 +1,13 @@
 '''
 DAY 1 Project : Band Name Generator
-Date : 18/10/2025
+    Date : 18/10/2025
+
 '''
 
-print()
+print("Welcome to the Band Name Generator.")
+
+city_name = input("What's the name of the city you grew up in?\n");
+pet_name = input("What's your pet's name?\n")
+
+print("Your band name could be " + city_name + " " + pet_name)
+
