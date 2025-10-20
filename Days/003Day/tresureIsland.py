3113c0e09aa6f28dd4842abd2a4a252b9531e83a
MomirCarevic <carevic.momir00@gmail.com>
2025-10-20 07:55:51 +0200

    Started final project for day 3

---
+++ Days/003Day/tresureIsland.py
@@ -0,0 +1,6 @@
+'''
+Todays final project is to make Tresure Island game.
+Date: 20/10/2025
+
+'''
+print("Welcome to Treasure Island. Your mission is to find the treasure.\nDo you go left or right?\n")
