c5c9abb4415290596620db5665f6db5f735c76ec
MomirCarevic <carevic.momir00@gmail.com>
2025-10-20 22:40:17 +0200

    Daily project finished

--- Days/004Day/rockPaperScissors.py
+++ Days/004Day/rockPaperScissors.py
@@ -28,3 +28,34 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+hand_sign = [rock, paper, scissors]
+
+player_1 = hand_sign[random.randint(0,2)]
+player_2 = hand_sign[random.randint(0,2)]
+
+print(player_1)
+print(player_2)
+
+if player_1 == rock :
+    if player_2 == paper :
+        print("PLAYER 2 WINS!")
+    elif player_2 == scissors :
+        print("PLAYER 1 WINS!")
+    else :
+        print("TIE")
+elif player_1 == paper :
+    if player_2 == scissors :
+        print("PLAYER 2 WINS!")
+    elif player_2 == rock :
+        print("PLAYER 1 WINS!")
+    else :
+        print("TIE")
+
+elif player_1 == scissors :
+    if player_2 == rock :
+        print("PLAYER 2 WINS!")
+    elif player_2 == paper :
+        print("PLAYER 1 WINS!")
+    else :
+        print("TIE")
