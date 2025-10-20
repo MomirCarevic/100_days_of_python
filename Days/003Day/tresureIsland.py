943dcfecacfff9ad756f3783c644c00fa8fc26a4
MomirCarevic <carevic.momir00@gmail.com>
2025-10-20 21:42:46 +0200

    First version of Tresure Island

--- Days/003Day/tresureIsland.py
+++ Days/003Day/tresureIsland.py
@@ -3,4 +3,21 @@ Todays final project is to make Tresure Island game.
 Date: 20/10/2025
 
 '''
-print("Welcome to Treasure Island. Your mission is to find the treasure.\nDo you go left or right?\n")
+which_way = input("Welcome to Treasure Island. Your mission is to find the treasure.\nYou are at a crossroad, wher do you go left or R right?\n")
+
+if which_way == "Left" or which_way == "left" :
+    swim_wait = input("Do you swim or wait\n")
+    if swim_wait == "Wait" or swim_wait == "wait" :
+        which_door = input("Which door color do you chose? Red, Blue or Yellow?\n")
+        if which_door == "Red" :
+            print("You were eaten by beast, GAME OVER!")
+        elif which_door == "Blue" :
+            print("You were burnt by fire, GAME OVER!")
+        elif which_door == "Yellow" :
+            print("YOU WIN!\n")
+        else :
+            print("GAME OVER!\n")
+    else:  
+        print("You were attacked by trout, GAME OVER!\n")
+else :
+    print("You fell into a hole, GAME OVER!\n")
