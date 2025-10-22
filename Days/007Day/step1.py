fc30dcaffeaa22371ebe3bb1bcfb2c68e4fb0162
MomirCarevic <carevic.momir00@gmail.com>
2025-10-22 20:28:35 +0200

    Finished TODO-2

--- Days/007Day/step1.py
+++ Days/007Day/step1.py
@@ -9,4 +9,12 @@ print(chosen_word)
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
+guess = input("Guess a letter: ")
+
+for n in range(0,len(chosen_word)) : 
+    if guess.lower() == chosen_word[n] :
+        print("Right")
+    else : 
+        print("Wrong")
+        
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it is, "Wrong" if it's not.
