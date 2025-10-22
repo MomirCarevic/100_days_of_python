5cf58426c9535c7adb265af9de34f181c4d10711
MomirCarevic <carevic.momir00@gmail.com>
2025-10-22 23:18:05 +0200

    Setting up Day 8 project : Ceasar Cipher

---
+++ Days/008Day/README.md
@@ -0,0 +1,3 @@
+# Caesar Cipher
+
+Caesar cipher is encoding by shifting our text. If the number of shifts is positive, we move our letters to left, if negative they move to right.
---
+++ Days/008Day/ceasarCipher.py
@@ -0,0 +1,7 @@
+# TODO-1 - Create a function called encrypt() that takes 'orginal_text' and 'shift_amount' as 2 inputs
+
+# TODO-2 - Inside the encrypt function shift each letter of the original_text forward in the algorithm by the shift_amount and print the encrypted text. You can use index() for finding out postion of an intem in a list
+
+choice = input("Type 'encode' to encrypt, type 'decode' to decrypt:")
+
+# TODO-3 - Call the enxrypt() function and pass the user inputs
