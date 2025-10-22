aedeae71cce6f9fe6e61762169dca9605597edba
MomirCarevic <carevic.momir00@gmail.com>
2025-10-22 21:54:57 +0200

    Finished task 61. Functions with Inputs

---
+++ Days/Exercise/greet.py
@@ -0,0 +1,15 @@
+# def greet():
+#     print("Hello")
+#     print("Hello")
+#     print("Hello")
+
+# greet()
+
+# Function with input
+
+def greetWithName(name) :
+    print(f"Hello {name}")
+    print(f"Hello {name}")
+    print(f"Hello {name}")
+
+greetWithName(input("what is your name"))
