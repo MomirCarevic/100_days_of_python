bbf5da590afe515c90ac9890b47f987861f13110
MomirCarevic <carevic.momir00@gmail.com>
2025-10-24 22:59:21 +0200

    Finished task 2, adding decryption

--- Days/008Day/ceasarCipher.py
+++ Days/008Day/ceasarCipher.py
@@ -1,9 +1,3 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n")
-shift = int(input("Type the shift number:\n"))
-
 def encrypt(original_text,shift_amount) :
     cipher_text =""
 
@@ -13,6 +7,25 @@ def encrypt(original_text,shift_amount) :
         cipher_text += alphabet[shifted_position]
 
     print(f"Encripted word is {cipher_text}") 
+    return cipher_text
+
+def decrypt(original_text,shift_amount) :
+    cipher_text = ""
+
+    for letter in original_text : 
+        shifted_position = alphabet.index(letter) - shift_amount
+        shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
+
+    print(f"Decrypted word is {cipher_text}")
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n")
+shift = int(input("Type the shift number:\n"))
 
 
-encrypt(original_text=text,shift_amount=shift)
+
+encrypted_text = encrypt(original_text=text,shift_amount=shift)
+decrypt(original_text=encrypted_text,shift_amount=shift)
