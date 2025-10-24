8926adae10dd47f0bf2622e19de021f9730176da
MomirCarevic <carevic.momir00@gmail.com>
2025-10-24 22:32:04 +0200

    Starting Ceasar Cypher daily project with right setup

--- Days/008Day/ceasarCipher.py
+++ Days/008Day/ceasarCipher.py
@@ -1,7 +1,15 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n")
+shift = input("Type the shift number:\n")
+
 # TODO-1 - Create a function called encrypt() that takes 'orginal_text' and 'shift_amount' as 2 inputs
+
+
 
 # TODO-2 - Inside the encrypt function shift each letter of the original_text forward in the algorithm by the shift_amount and print the encrypted text. You can use index() for finding out postion of an intem in a list
 
-choice = input("Type 'encode' to encrypt, type 'decode' to decrypt:")
+
 
 # TODO-3 - Call the enxrypt() function and pass the user inputs
