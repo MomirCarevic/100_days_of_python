b440f5298edb54044a2f84b40322c169682444ae
MomirCarevic <carevic.momir00@gmail.com>
2025-10-26 12:49:55 +0100

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   Days/011Day/blackjackCapstoneProject.py

--- Days/011Day/blackjackCapstoneProject.py
+++ Days/011Day/blackjackCapstoneProject.py
@@ -1,9 +1,18 @@
+import random
 from os import system
 from art import logo
+
+cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 
 start_game = input("Do you want to play a game of Blackjack? Type 'yes' or 'no'\n")
 
 while start_game == 'yes' :
     system("cls")
     print(logo)
-    #start_game = 'no'
+    
+    card1 = random.choice(cards)
+    card2 = random.choice(cards)
+    
+    print(card1,card2)
+
+    start_game = 'no'
