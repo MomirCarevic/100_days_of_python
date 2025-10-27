39d1f576cbd8921338f0d16f45bc46bda5fb3480
MomirCarevic <carevic.momir00@gmail.com>
2025-10-27 22:03:43 +0100

    Pushing program that is not working currently, will resume tomorrow

--- Days/014Day/higherLower.py
+++ Days/014Day/higherLower.py
@@ -4,10 +4,18 @@ from game_data import data
 score = 0
 guess = True
 
+def loadData(dictionary,random) :
+    #loading data into a dictionary
+    dictionary = {**data[random]}
+
+A = {}
+B = {}
+
 while guess == True :
     print(art.logo)
-    print(random.choice(game_data.data))
-
+    
+    print(random.choice(data))
+    loadData(dictionary=A,random=random.choice(data))
     Continue = input("Do you want to continue? 'yes' or 'no'")
 
     if Continue == 'yes' :
