0f233d4a37b53d82cb3fc20d97aa9b35a65e74bc
MomirCarevic <carevic.momir00@gmail.com>
2025-11-04 21:55:09 +0100

    Succesfully added loading data feature

--- Days/014Day/higherLower.py
+++ Days/014Day/higherLower.py
@@ -4,18 +4,36 @@ from game_data import data
 score = 0
 guess = True
 
-def loadData(dictionary,random) :
-    #loading data into a dictionary
-    dictionary = {**data[random]}
+def loadData(new_data) :
+    new_data = random.choice(data)
+    return new_data
+
+def printData(dict) :
+    print(f" {dict.get("name")}", end="")
+    if dict.get("description") != "None" :
+        print(f", a {dict.get("description")}",end="")
+    if dict.get("country") != "None" :
+        print(f", from {dict.get("country")}")
+
+def printQuestion(dict1,dict2) :
+    print("Compare A: ",end="") 
+    printData(dict=A)
+    print(art.vs)
+    print("Against A: ",end="") 
+    printData(dict=B)
 
 A = {}
 B = {}
 
 while guess == True :
     print(art.logo)
+
+    A = loadData(A)
+    B = loadData(B)
     
-    print(random.choice(data))
-    loadData(dictionary=A,random=random.choice(data))
+    printQuestion(dict1=A,dict2=B)
+
+
     Continue = input("Do you want to continue? 'yes' or 'no'")
 
     if Continue == 'yes' :
